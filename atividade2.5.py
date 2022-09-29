19768a2f1f6289ee37c5edd46ee9d7117baaddfd
Vandilma Candido <90811919+Vandilmacandido@users.noreply.github.com>
2022-09-29 17:48:37 -0300

    Add files via upload

---
+++ atividade2.5.py
@@ -0,0 +1,45 @@
+def soma():
+    x = float(input("Primeiro numero: "))
+    y = float(input("Segundo numero: "))
+    return print("Soma: ",x+y)
+
+def subtracao():
+    x = float(input("Primeiro numero: "))
+    y = float(input("Segundo numero: "))
+    return print("Subtracao: ",x-y)
+
+def multiplicacao():
+    x = float(input("Primeiro numero: "))
+    y = float(input("Segundo numero: "))
+    return print("Multiplicacao: ",x*y)
+
+def divisao():
+    x = float(input("Primeiro numero: "))
+    y = float(input("Segundo numero: "))
+    return print("Divisao: ",x/y)
+def sair():
+    print(' Tchau!')
+    exit()
+    
+opcao=1
+
+while opcao:
+    print("0. Sair ")
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicação")
+    print("4. Divisão ")
+
+    opcao = int(input("Opção: "))
+
+    if(opcao==1):
+        soma()
+    if(opcao==2):
+        subtracao()
+    if(opcao==3):
+        multiplicacao()
+    if(opcao==4):
+        divisao()
+    if(opcao==0):
+        sair()
+  
